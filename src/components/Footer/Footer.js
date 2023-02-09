bd2d14cb9e3d8381072396976956588e1a41dbab
ezaatama <ezatama33@gmail.com>
2023-02-09 15:48:54 +0700

    delete unused file on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,7 @@ import React from 'react'
 import style from './Footer.module.css'
 import { Link } from 'react-router-dom';
 import {
-  FaFacebook,
   FaInstagram,
-  FaYoutube,
-  FaTwitter,
   FaLinkedin
 } from 'react-icons/fa';
 
