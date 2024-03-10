9c02be3af1a929e64a2d5a93390f5d693763e705
ezaatama <ezatama33@gmail.com>
2024-03-10 19:44:40 +0700

    change name

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,33 +1,30 @@
-import React from 'react'
-import style from './Footer.module.css'
-import { Link } from 'react-router-dom';
-import {
-  FaInstagram,
-  FaLinkedin
-} from 'react-icons/fa';
+import React from "react";
+import style from "./Footer.module.css";
+import { Link } from "react-router-dom";
+import { FaInstagram, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
-    return (
-        <div className={style.footerSection}>
-            <div className={style.footerLinks}>
-                <div className={style.footerLinksWrapper}>
-                    <div className={style.footerLinksItems}>
-                        <h2>Social Media</h2>
-                        {/* <a
+  return (
+    <div className={style.footerSection}>
+      <div className={style.footerLinks}>
+        <div className={style.footerLinksWrapper}>
+          <div className={style.footerLinksItems}>
+            <h2>Social Media</h2>
+            {/* <a
                         className={style.socialIconLink}
                         href='#'
                         aria-label='Facebook'
                         >
                         <FaFacebook />
                         </a> */}
-                        <a
-                        className={style.socialIconLink}
-                        href='https://www.instagram.com/ezaatama'
-                        aria-label='Instagram'
-                        >
-                        <FaInstagram />
-                        </a>
-                        {/* <a
+            <a
+              className={style.socialIconLink}
+              href="https://www.instagram.com/ezaatama"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </a>
+            {/* <a
                         className={style.socialIconLink}
                         href={
                             '#'
@@ -43,30 +40,30 @@ function Footer() {
                         >
                         <FaTwitter />
                         </a> */}
-                        <a
-                        className={style.socialIconLink}
-                        href="https://www.linkedin.com/in/reza-putra-pratama/"
-                        aria-label='LinkedIn'
-                        >
-                        <FaLinkedin />
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <section className={style.sosmedSection}>
-                <div className={style.sosmedWrap}>
-                <div className={style.footerLogo}>
-                    <Link to='/' className={style.sosmedLogo}>
-                    CloudTama
-                    </Link>
-                </div>
-                <small className={style.websiteRights}>Reza Putra Pratama @2021</small>
-                
-                </div>
-            </section>
+            <a
+              className={style.socialIconLink}
+              href="https://www.linkedin.com/in/reza-putra-pratama/"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin />
+            </a>
+          </div>
         </div>
-    )
+      </div>
+      <section className={style.sosmedSection}>
+        <div className={style.sosmedWrap}>
+          <div className={style.footerLogo}>
+            <Link to="/" className={style.sosmedLogo}>
+              ezdevs
+            </Link>
+          </div>
+          <small className={style.websiteRights}>
+            Reza Putra Pratama @2021
+          </small>
+        </div>
+      </section>
+    </div>
+  );
 }
 
-
-export default Footer
+export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,54 +1,70 @@
-import React, { useState } from 'react'
-import style from './Navbar.module.css'
-import logo from '../images/eza.png'
-import { Link } from 'react-router-dom'
-import { FaBars, FaTimes } from 'react-icons/fa'
-import { IconContext } from 'react-icons/lib'
+import React, { useState } from "react";
+import style from "./Navbar.module.css";
+import logo from "../images/eza.png";
+import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
 
 function Navbar() {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click);
-    const closeMobileMenu = () => setClick(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
 
-    return (
-        <>
-        <IconContext.Provider value={{ color: '000'}} >
-            <div className={style.navbar}>
-                <div className={style.navbarContainer}>
-                    <Link to='/' className={style.navbarLogo} onClick={closeMobileMenu}>
-                        <img src={logo} alt="logo" className={style.navbarIcon} />
-                        CloudTama
-                    </Link>
-                    <div className={style.menuIcon} onClick={handleClick}>
-                        {click ? <FaTimes /> : <FaBars />}
-                    </div>
-                    <ul className={ click ? style.navMenuActive : style.navMenu }>
-                        <li className={style.navItem}>
-                            <Link to='/' className={style.navLinks} onClick={closeMobileMenu}>
-                                Home
-                            </Link>
-                        </li>
-                        <li className={style.navItem}>
-                            <Link to='/about' className={style.navLinks} onClick={closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>
-                        <li className={style.navItem}>
-                            <Link to='/portofolio' className={style.navLinks} onClick={closeMobileMenu}>
-                                Portofolio
-                            </Link>
-                        </li>
-                        <li className={style.navItem}>
-                            <Link to='/contact' className={style.navLinks} onClick={closeMobileMenu}>
-                                Contact
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+  return (
+    <>
+      <IconContext.Provider value={{ color: "000" }}>
+        <div className={style.navbar}>
+          <div className={style.navbarContainer}>
+            <Link to="/" className={style.navbarLogo} onClick={closeMobileMenu}>
+              <img src={logo} alt="logo" className={style.navbarIcon} />
+              ezdevs
+            </Link>
+            <div className={style.menuIcon} onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
             </div>
-        </IconContext.Provider>
-        </>
-    );
+            <ul className={click ? style.navMenuActive : style.navMenu}>
+              <li className={style.navItem}>
+                <Link
+                  to="/"
+                  className={style.navLinks}
+                  onClick={closeMobileMenu}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className={style.navItem}>
+                <Link
+                  to="/about"
+                  className={style.navLinks}
+                  onClick={closeMobileMenu}
+                >
+                  About
+                </Link>
+              </li>
+              <li className={style.navItem}>
+                <Link
+                  to="/portofolio"
+                  className={style.navLinks}
+                  onClick={closeMobileMenu}
+                >
+                  Portofolio
+                </Link>
+              </li>
+              <li className={style.navItem}>
+                <Link
+                  to="/contact"
+                  className={style.navLinks}
+                  onClick={closeMobileMenu}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </IconContext.Provider>
+    </>
+  );
 }
 export default Navbar;
